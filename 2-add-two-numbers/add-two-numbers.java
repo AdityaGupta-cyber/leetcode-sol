7d4d711f0104b7f4d5a6d9d2171bc43974fb84e4
Aditya Gupta <73712794+AdityaGupta-cyber@users.noreply.github.com>
2026-01-24 17:32:55 +0530

    Time: 1 ms (99.73%) | Memory: 45.8 MB (99.23%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -26,17 +26,6 @@ class Solution {
         while (node1 != null ||  node2 != null || carry !=0) {
             int sum = 0;
 
-            // if (node1 != null || node2 != null) {
-            //     sum = carry + node1.val + node2.val;
-            //     node1 = node1.next;
-            //     node2 = node2.next;
-            // } else if (node1 == null) {
-            //     sum = carry + node2.val;
-            //     node2 = node2.next;
-            // } else {
-            //     sum = carry + node1.val;
-            //     node1 = node1.next;
-            // }
 
             if(node1 != null ){
                 sum +=  node1.val;
@@ -54,67 +43,8 @@ class Solution {
             
         }
 
-        // while (node1 != null) {
-        //     int sum = 0;
-        //     sum = carry + node1.val;
-        //     node1 = node1.next;
-        //     carry = sum/10;
-        //     if (result == null) {
-        //         result = new ListNode(sum % 10);
-        //         resultHead = result;
-        //     } else {
-        //         result.next = new ListNode(sum % 10);
-        //         result = result.next;
-        //     }
-        // }
-
-        // while (node2 != null) {
-        //     int sum = 0;
-        //     sum = carry + node2.val;
-        //     node2 = node2.next;
-        //     carry = sum/10;
-        //         result.next = new ListNode(sum % 10);
-        //         result = result.next;
-        // }
-
-        // if (carry != 0) {
-        //     result.next = new ListNode(carry);
-        // }
 
         return resultHead.next;
     }
 
-    // Iterate through the to linkedLists and store it into the number 
-    // private int getNumberFromList(ListNode l) {
-    //     int temp = 0;
-    //     ListNode tempNode = l;
-    //     while (tempNode.next != null) {
-    //         temp = temp * 10 + l.val;
-    //         tempNode = l.next;
-    //     }
-    //     return temp;
-    // }
-
-    // private ListNode getListFromNumber(int number) {
-
-    //     if (number == 0) {
-    //         return new ListNode(0);
-    //     }
-
-    //     ListNode listHead = null;
-    //     ListNode listTail = null;
-    //     while (number > 0) {
-    //         if (listHead == null) {
-    //             listHead = new ListNode(number);
-    //             listTail = listHead;
-    //         }
-    //         else{
-    //             listTail.next = new ListNode(number);
-    //             listTail = listTail.next;
-    //         }
-
-    //         number = number / 10;
-    //     }
-    //     return listHead;
-    // }
 }
