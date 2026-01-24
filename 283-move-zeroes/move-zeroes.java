07c9973b224d27a40c07a9283edf8088fcc465e9
Aditya Gupta <73712794+AdityaGupta-cyber@users.noreply.github.com>
2026-01-24 20:08:57 +0530

    Time: 3 ms (20.12%) | Memory: 47.8 MB (46.81%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -10,14 +10,12 @@ class Solution {
 
             //now that the lastZeroPosition is initialized with an index;
             //we check if the number is a non zero and if it is we swap and lastZeroPosition +=1;
-            if(nums[i] != 0){
+            if(nums[i] == 0) continue;
                 //swap values;
                 int temp = nums[i];
                 nums[i] = nums[lastZeroPosition];
                 nums[lastZeroPosition] = temp;
                 lastZeroPosition = lastZeroPosition +1;
-            }
-            if(nums[i] == 0) continue;
         }
     }
 }
