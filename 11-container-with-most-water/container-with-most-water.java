c50729975149bb9dfc887f7daddd5479d161622f
Aditya Gupta <73712794+AdityaGupta-cyber@users.noreply.github.com>
2026-01-28 23:07:41 +0530

    Time: 5 ms (79.39%) | Memory: 77.2 MB (79.52%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,38 +1,22 @@
 class Solution {
-// brute force -  O(n * n)
-    //l * b or height * breadth
-    // check breadth between the pointers and the minimum of both the heights
-
-    // public int maxArea(int[] height) {
-    //     int area = 0;
-    //     for(int i = 0; i< height.length; i++){
-    //         for(int j = i; j < height.length; j++){
-    //             int calculateHeight = Math.min(height[i],height[j]);
-    //             area  = Math.max(area, (j - i) * calculateHeight);
-    //         }
-    //     }
-    //     return area;
-    // }
-
-
- // optimal - in linear time  using two  pointer 
- // two pointers on the extremes of the array and after calculation of the area the pointer which holds lesser height is moved to the next position
     public int maxArea(int[] height) {
-        int area  = 0;
-        int left  = 0;
-        int right = height.length - 1;
+        //two pointers;
 
-        while(left < right){
-            area =  Math.max(area,( right - left) * Math.min(height[left], height[right]));
+        int i = 0;
+        int j = height.length - 1;
+        
+        int max = Integer.MIN_VALUE;
+        while( i < j){
+            int area = (j - i) * Math.min(height[i], height[j]);
+            max = Math.max(area,max);
 
-            if( height[left] <= height[right])
-            {
-                left++;
+            if(height[i]<=height[j]){
+                i++;
             }
             else{
-                right--;
+                j--;
             }
         }
-        return area;
+        return max;
     }
 }
