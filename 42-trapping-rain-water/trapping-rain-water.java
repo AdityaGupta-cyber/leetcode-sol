fbb3147470a95a10dd6e4c79d288515cf1ff29c7
Aditya Gupta <73712794+AdityaGupta-cyber@users.noreply.github.com>
2026-01-31 19:47:59 +0530

    Time: 0 ms (100.00%) | Memory: 47.9 MB (45.23%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,78 +1,32 @@
-// class Solution {
-//     public int trap(int[] height) {
-//         //T.C -> O(n)
-//         //start with array pre-processing and store the max value of left and right based on the current index in the array.
-//         int[] leftMax = new int[height.length];
-//         int[] rightMax = new int[height.length];
-//         int count = 0;
-//         preProcessArray(leftMax, rightMax,height);
-
-//         for (int i = 0; i < height.length; i++) {
-//             int block = Math.min(leftMax[i],rightMax[i]) - height[i];
-//             if(block < 0) continue;
-//             count += block;
-//         }
-//         return count;
-//     }
-
-//     public static void preProcessArray(int[] leftMax, int[] rightMax,int[] height) {
-//         int left = 0;
-//         int right = 0;
-//         for (int i = 0; i < height.length; i++) {
-//             if (left < height[i]) {
-//                 left = height[i];
-//                 leftMax[i] = height[i];
-//             }
-//             else{
-//                 leftMax[i] = left;
-//             }
-//             if (right < height[(height.length - 1) - i]) {
-//                 right = height[(height.length - 1) - i];
-//                 rightMax[(height.length - 1) - i] = height[(height.length - 1) - i];
-//             }
-//             else{
-//                 rightMax[(height.length - 1) - i] = right;
-//             }
-//         }
-
-//     }
-// }
-
 class Solution {
     public int trap(int[] height) {
-        if (height == null || height.length == 0)
-            return 0;
-        int n = height.length;
-        int leftMax = height[0];
-        int rightMax = height[n - 1];
-
-        int countBlocks = 0;
-
-        int i = 1;
-        int j = n - 2;
+        int result = 0;
+        int maxRight = 0;
+        int maxLeft = 0;
+        int i = 0;
+        int j = height.length - 1;
 
         while (i <= j) {
-            if (leftMax <= rightMax) {
-                leftMax = Math.max(leftMax, height[i]);
-                countBlocks += (leftMax - height[i]);
+
+            if (height[i] <= height[j]) {
+                //check if the maxLeft is smaller than height[i]
+
+                if (height[i] >= maxLeft)
+                    maxLeft = height[i];
+
+                else
+                    result += maxLeft - height[i];
                 i++;
             } else {
-                rightMax = Math.max(rightMax, height[j]);
-                countBlocks += (rightMax - height[j]);
+                if (height[j] >= maxRight)
+                    maxRight = height[j];
+                else
+                    result += maxRight - height[j];
                 j--;
             }
         }
-        while (i <= j) {
-            if (leftMax <= rightMax) {
-                leftMax = Math.max(leftMax, height[i]);
-                countBlocks += leftMax - height[i];
-                i++;
-            } else {
-                rightMax = Math.max(rightMax, height[j]);
-                countBlocks += rightMax - height[j];
-                j--;
-            }
-        }
-        return countBlocks;
+
+        return result;
+
     }
 }
