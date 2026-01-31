3f04ad806bb141c7dbd825ce737ec23a9ec646df
Aditya Gupta <73712794+AdityaGupta-cyber@users.noreply.github.com>
2026-01-31 23:50:44 +0530

    Time: 1 ms (60.41%) | Memory: 48.8 MB (14.30%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,32 +1,27 @@
 class Solution {
     public int trap(int[] height) {
-        int result = 0;
-        int maxRight = 0;
+        int countBlocks = 0;
+        int[] prefixMax = new int[height.length];
+        int[] suffixMax = new int[height.length];
+
+        //max to the left of the index;
         int maxLeft = 0;
-        int i = 0;
-        int j = height.length - 1;
-
-        while (i <= j) {
-
-            if (height[i] <= height[j]) {
-                //check if the maxLeft is smaller than height[i]
-
-                if (height[i] >= maxLeft)
-                    maxLeft = height[i];
-
-                else
-                    result += maxLeft - height[i];
-                i++;
-            } else {
-                if (height[j] >= maxRight)
-                    maxRight = height[j];
-                else
-                    result += maxRight - height[j];
-                j--;
-            }
+        int maxRight = 0;
+        for (int i = 0; i < height.length; i++) {
+            maxLeft = Math.max(maxLeft, height[i]);
+            prefixMax[i] = maxLeft;
+        }
+        //max to the right of the index;
+        for (int i = height.length - 1; i >= 0; i--) {
+            maxRight = Math.max(maxRight, height[i]);
+            suffixMax[i] = maxRight;
         }
 
-        return result;
+        for (int i = 0; i < height.length; i++) {
+            int minHeight = Math.min(prefixMax[i], suffixMax[i]);
+            countBlocks += minHeight - height[i];
+        }
 
+        return countBlocks;
     }
 }
